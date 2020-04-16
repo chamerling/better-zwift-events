6582c78fb6a4500e9d4dc3086f16c7007ec0521b
Christophe HAMERLING <christophe.hamerling@gmail.com>
2020-04-16 23:07:48 +0200

    Add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="white">
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-help-circle</v-icon>
+    </v-app-bar>
+
+    <v-content>
+      <transition name="fade" mode="out-in">
+        <router-view />
+      </transition>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
 </style>
