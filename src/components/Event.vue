eb0749de9102911eb8ee01eaa7d5237e56c1db80
Christophe HAMERLING <chamerling@linagora.com>
2020-04-16 23:41:30 +0200

    Add zwift events

---
+++ src/components/Event.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="mx-auto" max-width="400">
+    <v-img class="white--text align-end" height="200px" :src="event.imageUrl">
+      <v-card-title>{{ event.name }}</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">{{ event.eventStart }}</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>Whitsunday Island, Whitsunday Islands</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="orange" text>
+        Details
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Event",
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="home">
-    Hello
+    <div id="event" class="mb-4" v-for="event in events" :key="event.id">
+      <Event :event="event" />
+    </div>
   </div>
 </template>
 
 <script>
+import Event from "@/components/Event.vue";
+
 export default {
-  name: "Home"
+  name: "Home",
+  data: () => ({
+    events: null
+  }),
+  mounted() {
+    this.axios
+      .get("https://us-or-rly101.zwift.com/api/public/events/upcoming?")
+      .then(response => response.data)
+      .then(events => (this.events = events))
+      .catch(err => console.log(err));
+  },
+  components: {
+    Event
+  }
 };
 </script>
