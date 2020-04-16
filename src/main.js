4e8597078e00ea6d938cf0ed2fd22435eca01f5c
Christophe HAMERLING <christophe.hamerling@gmail.com>
2020-04-16 23:12:49 +0200

    Add axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
