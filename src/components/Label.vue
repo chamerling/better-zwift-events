fb58ab0a8662691a0332cce037cfcf4fa6d313c1
Christophe HAMERLING <chamerling@linagora.com>
2020-04-18 23:30:01 +0200

    Add label

---
+++ src/components/Label.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-chip :color="color">
+    {{ label }}
+  </v-chip>
+</template>
+
+<script>
+const COLORS = {
+  A: "#fc4119",
+  B: "#58c34e",
+  C: "#3ec0e9",
+  D: "#fccf0b",
+  E: "#943e5e"
+};
+
+export default {
+  name: "Label",
+  props: {
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    color() {
+      return COLORS[this.label];
+    }
+  }
+};
+</script>
