a762ad87b446195f33374ceec09ca5e8e101d68f
Christophe HAMERLING <chamerling@linagora.com>
2020-04-27 18:26:26 +0200

    Add infinite loading

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,46 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { orderBy } from "lodash";
 
 Vue.use(Vuex);
 
+const PAGE_SIZE = 20;
+
+const state = {
+  events: {}
+};
+
+const actions = {
+  fetchEvents({ commit }) {
+    return Vue.axios
+      .get("https://us-or-rly101.zwift.com/api/public/events/upcoming?")
+      .then(({ data }) => {
+        commit("setEvents", data);
+      });
+  }
+};
+
+const mutations = {
+  setEvents(state, events) {
+    events.forEach(event => {
+      Vue.set(state.events, event.id, event);
+    });
+  }
+};
+
+const getters = {
+  getEvents: ({ events }) => (page = 0) => {
+    return orderBy(Object.values(events), "eventStart", "asc").slice(
+      page * PAGE_SIZE,
+      (page + 1) * PAGE_SIZE
+    );
+  }
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state,
+  mutations,
+  actions,
+  getters,
   modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,11 @@
       <div id="event" class="mb-4" v-for="event in events" :key="event.id">
         <Event :event="event" />
       </div>
+      <infinite-loading @infinite="loadMore">
+        <div slot="spinner">
+          <MessageEllipsis message="Loading" />
+        </div>
+      </infinite-loading>
     </div>
     <div v-else id="loader" class="d-flex justify-center align-center">
       <Loader />
@@ -12,26 +17,54 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import InfiniteLoading from "vue-infinite-loading";
 import Event from "@/components/Event.vue";
 import Loader from "@/components/Loader.vue";
+import MessageEllipsis from "@/components/MessageEllipsis.vue";
 
 export default {
   name: "Home",
   data: () => ({
-    events: null,
-    loaded: false
+    loaded: false,
+    page: 0,
+    events: []
   }),
   mounted() {
-    this.axios
-      .get("https://us-or-rly101.zwift.com/api/public/events/upcoming?")
-      .then(response => response.data)
-      .then(events => (this.events = events))
+    this.$store
+      .dispatch("fetchEvents")
+      .then(() => this.getFirstEvents())
       .then(() => (this.loaded = true))
       .catch(err => console.log(err));
   },
+  computed: {
+    ...mapGetters({ getEvents: "getEvents" })
+  },
+  methods: {
+    getFirstEvents() {
+      const events = this.getEvents(this.page);
+      this.events.push(...events);
+      this.page++;
+    },
+    loadMore($state) {
+      const events = this.getEvents(this.page);
+      this.page++;
+
+      setTimeout(() => {
+        if (events && events.length) {
+          this.events.push(...events);
+          $state && $state.loaded();
+        } else {
+          $state && $state.complete();
+        }
+      }, 600);
+    }
+  },
   components: {
     Event,
-    Loader
+    Loader,
+    InfiniteLoading,
+    MessageEllipsis
   }
 };
 </script>
