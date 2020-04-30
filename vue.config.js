4bccde3518b724070a6ff6f1560adb792621a5d6
Christophe HAMERLING <chamerling@linagora.com>
2020-04-30 14:08:22 +0200

    Update PWA icons and manifest

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   transpileDependencies: ["vuetify"],
 
   pwa: {
+    name: "Better Zwift Events",
+    themeColor: "#1eb980",
+    backgroundColor: "#33333d",
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
       swSrc: "./src/sw.js",
