5f0ab0a6467fcdc82958dc28194b08c406622cc0
Christophe HAMERLING <christophe.hamerling@gmail.com>
2020-04-30 09:51:23 +0200

    Remove router from analytics

--- src/plugins/analytics.js
+++ src/plugins/analytics.js
@@ -1,12 +1,10 @@
 import Vue from "vue";
 import VueGtag from "vue-gtag";
-import router from "@/router";
 
 const VUE_APP_GOOGLE_ANALYTICS_ID = process.env.VUE_APP_GOOGLE_ANALYTICS_ID;
 
 if (VUE_APP_GOOGLE_ANALYTICS_ID) {
   Vue.use(VueGtag, {
-    config: { id: VUE_APP_GOOGLE_ANALYTICS_ID },
-    router
+    config: { id: VUE_APP_GOOGLE_ANALYTICS_ID }
   });
 }
